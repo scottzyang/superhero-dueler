a489c136cf9583caf9c842294a9333261c6ffa26
scottzyang <scottzyang@gmail.com>
2022-11-13 11:55:17 -0600

    created dog class

---
+++ my_dogs.py
@@ -0,0 +1,11 @@
+from dog import Dog 
+
+my_dog = Dog("rex", "super dog")
+
+my_dog2 = Dog("Simba", "super super dog")
+my_dog3 = Dog("Kena", "the superest of dogs")
+my_dog4 = Dog("Khan", "The wildest of dogs")
+
+my_dog2.sit()
+my_dog3.roll()
+my_dog4.bark()
