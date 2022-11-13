f424ba69464ec79e312f34392f40c089d41ea42e
scottzyang <scottzyang@gmail.com>
2022-11-13 12:30:51 -0600

    Create Armor class with block method

---
+++ armor.py
@@ -0,0 +1,15 @@
+import random
+
+class Armor: 
+  def __init__(self, name, max_block):
+    self.name = name
+    self.max_block = max_block
+
+  def block(self):
+    block_effect = random.randint(0, self.max_block)
+    return block_effect
+
+if __name__ == "__main__":
+  armor = Armor("Debugging Shield", 10)
+  print(armor.name)
+  print(armor.block())
