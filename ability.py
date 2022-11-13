bb05511f0b849ebb690d60866485dfb31f6620d5
scottzyang <scottzyang@gmail.com>
2022-11-13 12:26:46 -0600

    Create Ability class with attack method

---
+++ ability.py
@@ -0,0 +1,16 @@
+import random
+
+class Ability:
+  def __init__(self, name, max_damage):
+    self.name = name
+    self.max_damage = max_damage
+
+  def attack(self):
+    total_damage = random.randint(0, self.max_damage)
+    return total_damage
+
+
+if __name__ == "__main__":
+  ability = Ability("Debugging Ability", 20)
+  print(ability.name)
+  print(ability.attack())
